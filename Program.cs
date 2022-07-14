20f016b248b0814c98c682deae035cca878adfc8
Nickpandiscia <npandiscia1@gmail.com>
2022-07-14 16:53:25 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(100);
+            Console.WriteLine($"Converting 100F to celsius gives us {celsius}C. ");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(100);
+            Console.WriteLine($"Converting 100C to fahrenheit gives us {fahrenheit}F.");
+            Console.ReadLine();
+        }
+    }
+}
